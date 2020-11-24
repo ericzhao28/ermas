7bc55543ad648afe019fbce0dad577a63ade521d
Eric Zhao <ericlzhao28@gmail.com>
2020-11-24 02:14:08 -0800

    Initialize source code repository

---
+++ main.py
@@ -0,0 +1,268 @@
+import torch
+import torch.nn as nn
+from torch.distributions import Categorical
+import gym
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+############## Hyperparameters ##############
+env_name = "Erics env"
+# creating environment
+env = TradingEnv()
+
+n_latent_var = 64  # number of variables in hidden layer
+update_timestep = 2000  # update policy every n timesteps
+lr = 0.001
+betas = (0.9, 0.999)
+gamma = 0.995  # discount factor
+K_epochs = 4  # update policy for K epochs
+eps_clip = 0.2  # clip parameter for PPO
+random_seed = 1
+lambda_lr = 20
+beta = 0.3
+ermas_eps = -0.01
+eval_episodes = 2
+unilateral_episodes = 3
+main_episodes = 5
+num_loops = 10
+
+history = {
+    "a1": [],
+    "a2": [],
+    "delta1": [],
+    "delta2": [],
+    "rewardp": [],
+    "reward1": [],
+    "reward2": [],
+}
+
+#############################################
+
+if random_seed:
+    torch.manual_seed(random_seed)
+    env.seed(random_seed)
+
+memory = Memory()
+memory1 = Memory()
+memory2 = Memory()
+dmemory = Memory()
+p_ppo = PPO(state_dim, action_dim_p, n_latent_var, lr, betas, gamma, K_epochs,
+            eps_clip, 0)
+a1_ppo = ERMAS_PPO(state_dim, action_dim_a1, n_latent_var, lr, betas, gamma,
+                   K_epochs, eps_clip, 1)
+a2_ppo = ERMAS_PPO(state_dim, action_dim_a2, n_latent_var, lr, betas, gamma,
+                   K_epochs, eps_clip, 2)
+a1_mppo = PPO(state_dim, action_dim_a1, n_latent_var, lr, betas, gamma,
+              K_epochs, eps_clip, 1)
+a2_mppo = PPO(state_dim, action_dim_a2, n_latent_var, lr, betas, gamma,
+              K_epochs, eps_clip, 2)
+
+a1_lambda = 5
+a2_lambda = 5
+
+# logging variables
+avg_length = 0
+timestep = 0
+
+# training loop
+i_episode = 0
+for i_episode in range(num_loops):
+    p_running_reward = 0.0
+    a1_running_reward = 0.0
+    a2_running_reward = 0.0
+    # Main loop
+    for _ in range(main_episodes):
+        state = env.reset()
+        for t in range(max_timesteps):
+            timestep += 1
+
+            # Running policy_old:
+            p_action = p_ppo.policy_old.act(state, memory)
+            a1_action = a1_ppo.policy_old.act(state, memory)
+            a2_action = a2_ppo.policy_old.act(state, memory)
+            state, rewards, done = env.step([p_action, a1_action, a2_action])
+            p_reward, a1_reward, a2_reward = rewards
+
+            # Saving reward and is_terminal:
+            memory.rewards[0].append(p_reward)
+            memory.rewards[1].append(a1_reward)
+            memory.rewards[2].append(a2_reward)
+            memory.is_terminals.append(done)
+
+            # update if its time
+            if timestep % update_timestep == 0:
+                adv = p_ppo.update(memory)
+                a1_ppo.update(memory, a1_lambda, adv)
+                a2_ppo.update(memory, a2_lambda, adv)
+                memory.clear_memory()
+                timestep = 0
+
+            p_running_reward += p_reward
+            a1_running_reward += a1_reward
+            a2_running_reward += a2_reward
+            if done:
+                break
+
+            for k, v in env.get_stats().items():
+                k = "Env " + k
+                if k not in history:
+                    history[k] = []
+                history[k].append(v)
+
+        avg_length += t
+
+    avg_length = int(avg_length / (max_timesteps * main_episodes))
+    p_running_reward = float(
+        (p_running_reward / (max_timesteps * main_episodes)))
+    a1_running_reward = float(
+        (a1_running_reward / (max_timesteps * main_episodes)))
+    a2_running_reward = float(
+        (a2_running_reward / (max_timesteps * main_episodes)))
+    print('Episode {} \t avg length: {} \t p reward: {}'.format(
+        i_episode, avg_length, p_running_reward))
+    history["rewardp"].append(p_running_reward)
+    history["reward1"].append(a1_running_reward)
+    history["reward2"].append(a2_running_reward)
+
+    # Before eval
+    a1_running_reward = 0
+    a2_running_reward = 0
+    dmemory.clear_memory()
+    for _ in range(eval_episodes):
+        state = env.reset()
+        for t in range(max_timesteps):
+            timestep += 1
+            p_action = p_ppo.policy_old.act(state, dmemory)
+            a1_action = a1_ppo.policy_old.act(state, dmemory)
+            a2_action = a2_ppo.policy_old.act(state, dmemory)
+            state, rewards, done = env.step([p_action, a1_action, a2_action])
+            p_reward, a1_reward, a2_reward = rewards
+            if done:
+                break
+            a1_running_reward += a1_reward
+            a2_running_reward += a2_reward
+
+    # A1 loop
+    a1_mppo.policy_old.load_state_dict(a1_ppo.policy.state_dict())
+    a1_mppo.policy.load_state_dict(a1_ppo.policy.state_dict())
+    for _ in range(unilateral_episodes):
+        state = env.reset()
+        for t in range(max_timesteps):
+            timestep += 1
+            p_action = p_ppo.policy_old.act(state, memory1)
+            a1_action = a1_mppo.policy_old.act(state, memory1)
+            a2_action = a2_ppo.policy_old.act(state, memory1)
+            state, rewards, done = env.step([p_action, a1_action, a2_action])
+            p_reward, a1_reward, a2_reward = rewards
+            memory1.rewards[1].append(a1_reward)
+            memory1.is_terminals.append(done)
+            if timestep % update_timestep == 0:
+                a1_mppo.update(memory1)
+                memory1.clear_memory()
+                timestep = 0
+            if done:
+                break
+
+    # A2 loop
+    a2_mppo.policy_old.load_state_dict(a2_ppo.policy.state_dict())
+    a2_mppo.policy.load_state_dict(a2_ppo.policy.state_dict())
+    for _ in range(unilateral_episodes):
+        state = env.reset()
+        for t in range(max_timesteps):
+            timestep += 1
+            p_action = p_ppo.policy_old.act(state, memory2)
+            a1_action = a1_ppo.policy_old.act(state, memory2)
+            a2_action = a2_mppo.policy_old.act(state, memory2)
+            state, rewards, done = env.step([p_action, a1_action, a2_action])
+            p_reward, a1_reward, a2_reward = rewards
+            memory2.rewards[2].append(a2_reward)
+            memory2.is_terminals.append(done)
+            if timestep % update_timestep == 0:
+                a2_mppo.update(memory2)
+                memory2.clear_memory()
+                timestep = 0
+            if done:
+                break
+
+    # A1 eval
+    a1_new_running_reward = 0
+    dmemory.clear_memory()
+    for _ in range(eval_episodes):
+        state = env.reset()
+        for t in range(max_timesteps):
+            timestep += 1
+            p_action = p_ppo.policy_old.act(state, dmemory)
+            a1_action = a1_mppo.policy_old.act(state, dmemory)
+            a2_action = a2_ppo.policy_old.act(state, dmemory)
+            state, rewards, done = env.step([p_action, a1_action, a2_action])
+            p_reward, a1_reward, a2_reward = rewards
+            if done:
+                break
+            a1_new_running_reward += a1_reward
+
+    # A2 eval
+    a2_new_running_reward = 0
+    dmemory.clear_memory()
+    for _ in range(eval_episodes):
+        state = env.reset()
+        for t in range(max_timesteps):
+            timestep += 1
+            p_action = p_ppo.policy_old.act(state, dmemory)
+            a1_action = a1_ppo.policy_old.act(state, dmemory)
+            a2_action = a2_mppo.policy_old.act(state, dmemory)
+            state, rewards, done = env.step([p_action, a1_action, a2_action])
+            p_reward, a1_reward, a2_reward = rewards
+            if done:
+                break
+            a2_new_running_reward += a2_reward
+
+    # Apply meta learning updates
+    new_state_dict = a1_mppo.policy.state_dict()
+    old_state_dict = a1_ppo.policy.state_dict()
+    for key in new_state_dict:
+        old_state_dict[key] = (old_state_dict[key] - beta * a1_lambda *
+                               new_state_dict[key]) / (1 + beta * a1_lambda)
+
+    new_state_dict = a2_mppo.policy.state_dict()
+    old_state_dict = a2_ppo.policy.state_dict()
+    for key in new_state_dict:
+        old_state_dict[key] = (old_state_dict[key] - beta * a2_lambda *
+                               new_state_dict[key]) / (1 + beta * a2_lambda)
+
+    # Update lambda estimates
+    a1_lambda = a1_lambda + (lambda_lr / max_timesteps) * (
+        a1_new_running_reward - ermas_eps - a1_running_reward)
+    a2_lambda = a2_lambda + (lambda_lr / max_timesteps) * (
+        a2_new_running_reward - ermas_eps - a2_running_reward)
+
+    a1_lambda = max(0, a1_lambda)
+    a2_lambda = max(0, a2_lambda)
+
+    history["delta1"].append(a1_new_running_reward - a1_running_reward)
+    history["delta2"].append(a2_new_running_reward - a2_running_reward)
+    history["a1"].append(a1_lambda)
+    history["a2"].append(a2_lambda)
+    for k, v in env.get_stats().items():
+        k = "Env " + k
+        if k not in history:
+            history[k] = []
+        history[k].append(v)
+
+    # Log
+    a1_running_reward = float(
+        (a1_running_reward / (max_timesteps * eval_episodes)))
+    a1_new_running_reward = float(
+        (a1_new_running_reward / (max_timesteps * eval_episodes)))
+    print('Episode {} \t a1 reward: {}, {}'.format(i_episode,
+                                                   a1_running_reward,
+                                                   a1_new_running_reward))
+
+    a2_running_reward = float(
+        (a2_running_reward / (max_timesteps * eval_episodes)))
+    a2_new_running_reward = float(
+        (a2_new_running_reward / (max_timesteps * eval_episodes)))
+    print('Episode {} \t a2 reward: {}, {}'.format(i_episode,
+                                                   a2_running_reward,
+                                                   a2_new_running_reward))
+
+    print("Episode {} \t Lambdas".format(i_episode), a1_lambda, a2_lambda)
---
+++ ppo.py
@@ -0,0 +1,137 @@
+class Memory:
+    def __init__(self):
+        self.actions = [[], [], []]
+        self.logprobs = [[], [], []]
+        self.rewards = [[], [], []]
+        self.states = []
+        self.is_terminals = []
+
+    def clear_memory(self):
+        for c in self.actions:
+            del c[:]
+        for c in self.rewards:
+            del c[:]
+        for c in self.logprobs:
+            del c[:]
+        del self.states[:]
+        del self.is_terminals[:]
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, n_latent_var, index):
+        super(ActorCritic, self).__init__()
+
+        self.index = index
+
+        # actor
+        self.action_layer = nn.Sequential(
+            nn.Linear(state_dim, n_latent_var), nn.Tanh(),
+            nn.Linear(n_latent_var, n_latent_var), nn.Tanh(),
+            nn.Linear(n_latent_var, action_dim), nn.Softmax(dim=-1))
+
+        # critic
+        self.value_layer = nn.Sequential(nn.Linear(state_dim, n_latent_var),
+                                         nn.Tanh(),
+                                         nn.Linear(n_latent_var, n_latent_var),
+                                         nn.Tanh(), nn.Linear(n_latent_var, 1))
+
+    def forward(self):
+        raise NotImplementedError
+
+    def act(self, state, memory):
+        state = torch.from_numpy(state).float().to(device)
+        action_probs = self.action_layer(state)
+        dist = Categorical(action_probs)
+        action = dist.sample()
+
+        if self.index == 0:
+            memory.states.append(state)
+        memory.actions[self.index].append(action)
+        memory.logprobs[self.index].append(dist.log_prob(action))
+
+        return action.item()
+
+    def evaluate(self, state, action):
+        action_probs = self.action_layer(state)
+        dist = Categorical(action_probs)
+
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+
+        state_value = self.value_layer(state)
+
+        return action_logprobs, torch.squeeze(state_value), dist_entropy
+
+
+class PPO:
+    def __init__(self, state_dim, action_dim, n_latent_var, lr, betas, gamma,
+                 K_epochs, eps_clip, index):
+        self.lr = lr
+        self.betas = betas
+        self.gamma = gamma
+        self.eps_clip = eps_clip
+        self.K_epochs = K_epochs
+        self.index = index
+
+        self.policy = ActorCritic(state_dim, action_dim, n_latent_var,
+                                  index).to(device)
+        self.optimizer = torch.optim.Adam(self.policy.parameters(),
+                                          lr=lr,
+                                          betas=betas)
+        self.policy_old = ActorCritic(state_dim, action_dim, n_latent_var,
+                                      index).to(device)
+        self.policy_old.load_state_dict(self.policy.state_dict())
+
+        self.MseLoss = nn.MSELoss()
+
+    def update(self, memory):
+        # Monte Carlo estimate of state rewards:
+        rewards = []
+        discounted_reward = 0
+        for reward, is_terminal in zip(reversed(memory.rewards[self.index]),
+                                       reversed(memory.is_terminals)):
+            if is_terminal:
+                discounted_reward = 0
+            discounted_reward = reward + (self.gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+
+        # Normalizing the rewards:
+        rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+
+        # convert list to tensor
+        old_states = torch.stack(memory.states).to(device).detach()
+        old_actions = torch.stack(
+            memory.actions[self.index]).to(device).detach()
+        old_logprobs = torch.stack(
+            memory.logprobs[self.index]).to(device).detach()
+
+        first_advantage = None
+
+        # Optimize policy for K epochs:
+        for _ in range(self.K_epochs):
+            # Evaluating old actions and values:
+            logprobs, state_values, dist_entropy = self.policy.evaluate(
+                old_states, old_actions)
+
+            # Finding the ratio (pi_theta / pi_theta__old):
+            ratios = torch.exp(logprobs - old_logprobs.detach())
+
+            # Finding Surrogate Loss:
+            advantages = rewards - state_values.detach()
+            if first_advantage is None:
+                first_advantage = advantages
+            surr1 = ratios * advantages
+            surr2 = torch.clamp(ratios, 1 - self.eps_clip,
+                                1 + self.eps_clip) * advantages
+            loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(
+                state_values, rewards) - 0.05 * dist_entropy
+
+            # take gradient step
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
+
+        # Copy new weights into old policy:
+        self.policy_old.load_state_dict(self.policy.state_dict())
+        return first_advantage
